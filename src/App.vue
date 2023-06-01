2a6d90a1e7df5ec09746afac171cc0d08a339f3e
Kanchana Kariyawasam <kanchana@geveo.com>
2023-06-01 11:25:40 +0530

    created a bootstrap-vue login page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <LoginPage/>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import LoginPage from './components/LoginPage.vue';
+export default {
+  name: 'App',
+  components: {
+    LoginPage
+    
+}
+}
+</script>
 
 <style lang="scss">
 #app {
